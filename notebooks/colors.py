214d449370399ed34a12e18efa1a1cf1a3230e8b
Elias J Severinsen <elias.j.severinsen@gmail.com>
2025-11-14 14:55:46 +0100

    Add new color palette

--- notebooks/colors.py
+++ notebooks/colors.py
@@ -2,21 +2,40 @@
 Color palettes used in this paper.
 """
 
-colors5 = [
-    "#648FFF",
-    "#785EF0",
-    "#DC267F",
-    "#FE6100",
-    "#FFB000"
-]
+color_palette = "IBM"
 
-colors3 = [
-    "#648FFF",
-    "#785EF0",
-    "#DC267F",
-]
+if color_palette == "IBM":
+    colors5 = [
+        "#648FFF",
+        "#785EF0",
+        "#DC267F",
+        "#FE6100",
+        "#FFB000"
+    ]
+    colors3 = [
+        "#648FFF",
+        "#785EF0",
+        "#DC267F",
+    ]
+    colors2 = [
+        "#648FFF",
+        "#DC267F"
+    ]
 
-colors2 = [
-    "#648FFF",
-    "#DC267F"
-]
+elif color_palette == "midnight sky":
+    colors5 = [
+        "#00296b",
+        "#003f88",
+        "#00509d",
+        "#fdc500",
+        "#ffd500"
+    ]
+    colors3 = [
+        "#00296b",
+        "#00509d",
+        "#ffd500"
+    ]
+    colors2 = [
+        "#00296b",
+        "#fdc500"
+    ]
